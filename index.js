8787285a153f11bc7f9e4f19bdc75a1c8e7c7e68
Jerry Pallath <pallathj@gmail.com>
2018-01-29 15:54:17 -0500

    Final Touches to basic UI

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ var todoRoutes = require('./routes/todos')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 app.get('/', function(req,res){
-  res.send("Hello from the root route")
+  res.sendFile("index.html")
 });
 
 app.use('/api/todos', todoRoutes);
---
+++ public/app.js
@@ -0,0 +1,79 @@
+
+$(document).ready(function(){
+  $.getJSON("/api/todos")
+  .then(addTodos)
+});
+
+$('#todoInput').keypress(function(event){
+  if (event.which == 13) {
+    createTodo();
+  }
+});
+$('.list').on('click', 'span', function(e){
+  e.stopPropagation();
+  removeTodo($(this).parent());
+})
+
+$('.list').on('click', 'li', function(){
+  updateTodo($(this));
+})
+
+function addTodos(todos) {
+  //add todos to page here
+  todos.forEach(function(todo){
+    addTodo(todo);
+  })
+}
+function addTodo(todo){
+  var newTodo = $('<li class="task">' +todo.name + "<span>X</span></li>");
+  newTodo.data('id', todo._id);
+  newTodo.data('completed', todo.completed);
+  if (todo.completed) {
+    newTodo.addClass("done");
+  }
+  $('.list').append(newTodo);
+}
+
+function createTodo(){
+  var usrInput = $('#todoInput').val();
+  $.post('/api/todos', {name: usrInput})
+  .then(function(newTodo){
+    $('#todoInput').val('');
+    addTodo(newTodo)
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+}
+
+function removeTodo(todo){
+  var clickedId = todo.data('id');
+  var deleteURL = '/api/todos/' + clickedId
+  // $(this).parent().remove();
+  $.ajax({
+    method: 'DELETE',
+    url: deleteURL
+  })
+  .then(function(data){
+    todo.remove();
+  })
+  .catch(function(err){
+    console.log(err);
+  }
+  )}
+
+  function updateTodo(todo){
+    var updateURL = 'api/todos/' + todo.data('id');
+    var isDone = !todo.data('completed');
+    var updateData = {completed: isDone}
+    console.log(updateData);
+    $.ajax({
+      method: 'PUT',
+      url: updateURL,
+      data: updateData
+    })
+    .then(function(updatedTodo){
+      todo.toggleClass("done");
+      todo.data("completed", isDone);
+    })
+    }
